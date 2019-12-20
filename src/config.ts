0386215b07c980ef5caa1845d3f844dd092ed604
lichenyang <wulichenyang@163.com>
2019-12-20 12:05:09 +0800

    :sparkles: Add modules and myTestPage

---
+++ src/config.ts
@@ -0,0 +1,19 @@
+/**
+ * App config
+ * @file App 配置
+ * @module app/config
+ * @author Surmon <https://github.com/surmon-china>
+ */
+
+import { Platform } from 'react-native'
+
+export const appName = 'Surmon.me'
+export const webUrl = 'https://surmon.me'
+export const appApi = 'https://api.surmon.me'
+export const staticApi = 'https://cdn.surmon.me'
+export const gravatarApi = 'https://static.surmon.me/avatar'
+export const navigationPersistenceKey =  '___NavigationStateDEV__'
+
+export const IS_DEV = 1
+export const IS_IOS = Object.is(Platform.OS, 'ios')
+export const IS_ANDROID = !IS_IOS
